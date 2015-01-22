e1d9e9495849761eb9fcc8b64aa4e495dbad0801
Rémi Benoit <r3m1.benoit@gmail.com>
2015-01-22 11:03:32 +0100

    Start separate picture and start actovoties

--- app/src/main/java/com/ups/m2dl/colorblinder/StartActivity.java
+++ app/src/main/java/com/ups/m2dl/colorblinder/StartActivity.java
@@ -1,17 +1,75 @@
 package com.ups.m2dl.colorblinder;
 
+import android.content.ContentResolver;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.drawable.BitmapDrawable;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.io.File;
+import java.sql.Timestamp;
+import java.util.Date;
 
 
 public class StartActivity extends ActionBarActivity {
+    private static final String BASE_PATH = "Colorblinder/photos/";
+    private static final int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 100;
+    private Uri imageUri;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+
+        takePhoto();
+    }
+
+    public void takePhoto() {
+        String storageDirectory = BASE_PATH + new Timestamp(new Date().getTime()) + ".jpg";
+
+        File photo = new File(Environment.getExternalStorageDirectory(), storageDirectory);
+        photo.getParentFile().mkdirs();
+
+        imageUri = Uri.fromFile(photo);
+
+        Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
+        intent.putExtra(MediaStore.EXTRA_OUTPUT,imageUri);
+
+        startActivityForResult(intent, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        Toast.makeText(this, "Failed to load", Toast.LENGTH_SHORT).show();
+        if (requestCode == CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE) {
+            if (resultCode == RESULT_OK) {
+
+                Uri selectedImage = imageUri;
+                getContentResolver().notifyChange(selectedImage, null);
+                ContentResolver cr = getContentResolver();
+                Bitmap bitmap;
+                try {
+                    Intent intent = new Intent(this, LineActivity.class);
+                    intent.putExtra(LineActivity.CST_IMGURI, imageUri);
+                    startActivity(intent);
+
+                } catch (Exception e) {
+                    Toast.makeText(this, "Failed to load", Toast.LENGTH_SHORT).show();
+                    Log.e("Camera", e.toString());
+                }
+            }
+        }
     }
 
 
